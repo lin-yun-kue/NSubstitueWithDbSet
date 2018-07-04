232b07d828f1d780d9fbc36676b2e9477c868e2e
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2018-07-04 16:44:48 +0800

    加入專案檔案。

---
+++ NSubstitueWithDbSet/StudentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSubstitueWithDbSet
+{
+    public class StudentService
+    {
+        public DevEntities db;
+
+        public StudentService(DevEntities db)
+        {
+            this.db = db;
+        }
+
+        public void GetStuden()
+        {
+            var tmp = db.Student.Where(x => true);
+            return;
+        }
+    }
+}
---
+++ NSubstitueWithDbSetTests/StudentServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace NSubstitueWithDbSet.Tests
+{
+    [TestClass()]
+    public class StudentServiceTests
+    {
+        [TestMethod()]
+        public void GetStudenTest()
+        {
+            var mockDbSet = Substitute.For<DbSet<Student>, IQueryable<Student>>();
+            var mockEntity = Substitute.For<DevEntities>();
+
+            var students = new List<Student>()
+            {
+                new Student{ ID = 1, Name = "Kyle"},
+                new Student{ ID = 2, Name = "Zeal"}
+            };
+
+            var queryStudent = students.AsQueryable();
+
+            mockDbSet.AsQueryable().Provider.Returns(queryStudent.Provider);
+            mockDbSet.AsQueryable().Expression.Returns(queryStudent.Expression);
+            mockDbSet.AsQueryable().ElementType.Returns(queryStudent.ElementType);
+            mockDbSet.AsQueryable().GetEnumerator().Returns(queryStudent.GetEnumerator());
+
+            mockEntity.Student = mockDbSet;
+
+            var studentService = new StudentService(mockEntity);
+
+            studentService.GetStuden();
+        }
+    }
+}
